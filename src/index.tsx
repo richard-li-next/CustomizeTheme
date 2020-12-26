60a7ea8e0209fe0e16f6d7d7f6cea429cd5f39b1
richard-li-next <richard.li@nexttrucking.com>
2020-12-26 11:55:06 +0800

    add component

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'antd/dist/antd.less';
 import 'antd/lib/style/themes/default.less';
+import 'antd/dist/antd.less';
+import './theme.less';
 import App from './app';
 
 ReactDOM.render(<App />, document.getElementById('root'));
